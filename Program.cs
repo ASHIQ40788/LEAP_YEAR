b48101f08a19b2ecaecdca3df7b7fe444d53bd00
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-07 16:51:17 +0530

    [ASHIQ] added: LEAP YEAR

--- Program.cs
+++ Program.cs
@@ -7,15 +7,13 @@ namespace ProgramToCheckLeapYear
         static void Main(string[] args)
         {
             int year;
-            Console.Write("Enter the Year :");
+            Console.Write("Enter year :");
             year = Convert.ToInt32(Console.ReadLine());
             if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
                 Console.WriteLine("Year is Leap Year");
             else
                 Console.WriteLine("Year is not a Leap Year");
-
-
-            Console.ReadKey();
+                Console.ReadKey();
         }
     }
 }
